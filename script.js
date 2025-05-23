e4505f5ae1c381dbfa47c87fba9ee3699f3f9f75
Stokholmer <sofie_stokholm@hotmail.com>
2025-05-23 18:22:08 +0200

    ,

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function toggleMenu() {
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("navbarBurger");
   const overlay = document.getElementById("overlay");
-  overlay.style.width = (overlay.style.width === "100%") ? "0" : "100%";
-}
+  const closeBtn = document.getElementById("overlayClose");
+
+  burger.addEventListener("click", () => {
+    overlay.hidden = false;
+    overlay.style.width = "100%";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    overlay.style.width = "0";
+    setTimeout(() => {
+      overlay.hidden = true;
+    }, 300);
+  });
+});
