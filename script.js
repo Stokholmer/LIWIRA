3702c6a51dc9f9798d133394f34f642c1caf0969
Stokholmer <sofie_stokholm@hotmail.com>
2025-05-27 22:06:41 +0200

    Endelige endelige

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function filterSelection(category) {
 
   for (let i = 0; i < items.length; i++) {
     const item = items[i];
-    item.style.display = (category === "drinks__alle" || item.classList.contains(category)) ? "flex" : "none";
+
+    if (category === "drinks__alle" || item.classList.contains(category)) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
   }
 }
 
@@ -18,12 +23,15 @@ function initFilterButtons() {
 
   const btns = container.getElementsByClassName("drinks__btn");
 
-  for (let btn of btns) {
-    btn.addEventListener("click", function () {
-      for (let b of btns) {
-        b.classList.remove("active");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+
+      for (let j = 0; j < btns.length; j++) {
+        btns[j].classList.remove("active");
       }
+
       this.classList.add("active");
+
       const category = this.getAttribute("data-filter");
       filterSelection(category);
     });
